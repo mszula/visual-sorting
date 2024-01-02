8ae6135af2817f7bbba08cbd3e598336ab844290
Mateusz Szuła <mateusz.szulaa@gmail.com>
2024-01-03 00:50:03 +0100

    Sound improvement 🔈

--- src/lib/sound.ts
+++ src/lib/sound.ts
@@ -1,7 +1,7 @@
-import { Oscillator, Volume } from "tone";
+import { Synth } from "tone";
 import type { Time } from "tone/build/esm/core/type/Units";
 
-let oscilator: Oscillator;
+let synth = new Synth().toDestination();
 let freqStepSize = 0;
 
 const maxFrequency = 1500;
@@ -9,22 +9,15 @@ const minFrequency = 100;
 
 export const soundStart = (size: number) => {
   freqStepSize = maxFrequency / size;
-
-  oscilator = new Oscillator().connect(new Volume(-20).toDestination());
-  oscilator.type = "sawtooth";
-  oscilator.start();
+  synth.triggerAttack(0);
 };
 
 export const soundStop = (time?: Time) => {
-  if (oscilator) {
-    oscilator.stop(time);
-  }
+  synth.triggerRelease(time);
 };
 
 export const playValue = (value: number) => {
-  if (oscilator) {
-    const freq = freqStepSize * (value - 1) + minFrequency;
+  const freq = freqStepSize * (value - 1) + minFrequency;
 
-    oscilator.frequency.value = freq;
-  }
+  synth.oscillator.frequency.value = freq;
 };
