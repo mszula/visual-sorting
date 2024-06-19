7c743fe5dcdf85b104c4c28a7e6b8f83efd7fa2a
Mateusz Szuła <mateusz.szulaa@gmail.com>
2024-06-19 10:19:26 +0200

    Added Google Analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import * as child_process from "node:child_process";
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import * as child_process from 'node:child_process';
+import { VitePluginRadar } from 'vite-plugin-radar';
 
 if (process.env.VITE_GIT_COMMIT_REF === undefined) {
   process.env.VITE_GIT_COMMIT_REF = child_process
-    .execSync("git rev-parse --short HEAD")
+    .execSync('git rev-parse --short HEAD')
     .toString();
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-C25VS7L111',
+      },
+    }),
+  ],
 });
