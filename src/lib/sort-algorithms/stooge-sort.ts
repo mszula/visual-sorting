adc3d8ee61a14d130d6149f5ae86def124d072bd
Mateusz Szuła <mateusz.szulaa@gmail.com>
2024-06-27 11:26:12 +0200

    Stooge sort better visualization

--- src/lib/sort-algorithms/stooge-sort.ts
+++ src/lib/sort-algorithms/stooge-sort.ts
@@ -1,38 +1,31 @@
 import type { SortingGenerator } from './types';
 
-function* stoogeSortRun(arr: number[], l: number, h: number): SortingGenerator {
-  if (l >= h) return;
-
-  yield { access: [l, h], sound: l };
-
-  // If first element is smaller
-  // than last, swap them
-  if (arr[l] > arr[h]) {
-    const t = arr[l];
-    arr[l] = arr[h];
-    arr[h] = t;
+function* sort(arr: number[], l: number, h: number): SortingGenerator {
+  if (l >= h) {
+    return;
   }
 
-  // If there are more than 2
-  // elements in the array
+  // If first element is smaller than last, swap them
+  if (arr[l] > arr[h]) {
+    [arr[l], arr[h]] = [arr[h], arr[l]];
+    yield { access: [l, h], sound: l };
+  }
+
+  // If there are more than two elements in the array
   if (h - l + 1 > 2) {
     const t = Math.floor((h - l + 1) / 3);
 
-    // Recursively sort first
-    // 2/3 elements
-    yield* stoogeSortRun(arr, l, h - t);
+    // Recursively sort first 2/3 elements
+    yield* sort(arr, l, h - t);
 
-    // Recursively sort last
-    // 2/3 elements
-    yield* stoogeSortRun(arr, l + t, h);
+    // Recursively sort last 2/3 elements
+    yield* sort(arr, l + t, h);
 
-    // Recursively sort first
-    // 2/3 elements again to
-    // confirm
-    yield* stoogeSortRun(arr, l, h - t);
+    // Recursively sort first 2/3 elements again to confirm
+    yield* sort(arr, l, h - t);
   }
 }
 
 export const stoogeSort = function* (arr: number[]): SortingGenerator {
-  yield* stoogeSortRun(arr, 0, arr.length - 1);
+  yield* sort(arr, 0, arr.length - 1);
 };
